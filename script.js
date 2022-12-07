c8dabf6125c15d88c3f9c00cad19dc5588a3ba5e
vazirabonu [FE16] <102415721+vazirabonu23@users.noreply.github.com>
2022-12-07 15:11:08 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+
+
+    function setInfo()
+    {let student1 = {
+       name: prompt('Ism kiriting'),
+       surname : prompt('Familiya kiriting'),
+       age : prompt('Yosh kiriting'),
+       object: {student1:""
+       }}
+       console.log(student1);
+   
+       let student2 = {
+       name: prompt('Ism kiriting'),
+       surname : prompt('Familiya kiriting'),
+       age : prompt('Yosh kiriting'),
+       object: {student1:""
+       }}
+       console.log(student2);
+   
+       let student3 = {
+       name: prompt('Ism kiriting'),
+       surname : prompt('Familiya kiriting'),
+       age : prompt('Yosh kiriting'),
+       object: {student1:""
+       }}
+       console.log(student3);
+   
+       let student4 = {
+       name: prompt('Ism kiriting'),
+       surname : prompt('Familiya kiriting'),
+       age : prompt('Yosh kiriting'),
+       object: {student1:""
+       }}
+       console.log(student4);
+   
+   
+       let student5 = {
+           name: prompt('Ism kiriting'),
+           surname : prompt('Familiya kiriting'),
+           age : prompt('Yosh kiriting'),
+           object: {student1:""
+               }}
+               console.log(student5);
+   }
+   setInfo()
